c18fbb03975edb0256e39e4dcda8849d7c2777cd
lil-runa <runascandal0327@gmail.com>
2023-02-18 15:35:06 +0900

    投稿編集にバリデーションをかけました。投稿を削除する際にモーダルで表示させるようにしました。

--- resources/views/authenticated/bulletinboard/post_detail.blade.php
+++ resources/views/authenticated/bulletinboard/post_detail.blade.php
@@ -9,7 +9,7 @@
           </div>
           <div>
             <span class="edit-modal-open" post_title="{{ $post->post_title }}" post_body="{{ $post->post }}" post_id="{{ $post->id }}">編集</span>
-            <a href="{{ route('post.delete', ['id' => $post->id]) }}">削除</a>
+            <a href="{{ route('post.delete', ['id' => $post->id]) }}" onclick="return confirm('投稿を削除しますか？')">削除</a>
           </div>
         </div>
 
@@ -57,12 +57,16 @@
   <div class="modal__content">
     <form action="{{ route('post.edit') }}" method="post">
       <div class="w-100">
-        @if($errors->first('post_title'))
         <div class="modal-inner-title w-50 m-auto">
-        @endif
+          @if($errors->first('post_title'))
+          <span class="error_message">{{ $errors->first('post_title') }}</span>
+          @endif
           <input type="text" name="post_title" placeholder="タイトル" class="w-100">
         </div>
         <div class="modal-inner-body w-50 m-auto pt-3 pb-3">
+          @if($errors->first('post_body'))
+          <span class="error_message">{{ $errors->first('post_body') }}</span>
+          @endif
           <textarea placeholder="投稿内容" name="post_body" class="w-100"></textarea>
         </div>
         <div class="w-50 m-auto edit-modal-btn d-flex">
